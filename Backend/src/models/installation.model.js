455d6280927775c4fb6584cad16bd6e64ad01b03
Hatim Malak <hatim05042006@gmail.com>
2026-07-13 11:38:38 +0530

    feat: Add installation model with schema for installationId, accountLogin, and accountType

---
+++ Backend/src/models/installation.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const installationSchema = new mongoose.Schema(
+    {
+        installationId:{
+            type:Number,
+            required:true,
+            unique:true
+        },
+        accountLogin:{
+            type:String,
+            require:true,
+        },
+        accountType:{
+            type:String,
+            enum:["User","Organization"],
+        },
+    },
+    {timestamps:true}
+);
+
+const Installation = mongoose.model("Installation",installationSchema)
+export default Installation
