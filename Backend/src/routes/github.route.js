d5936c89a550edf27c54692a1a0c1178800f2c9c
Hatim Malak <hatim05042006@gmail.com>
2026-07-14 23:52:07 +0530

    feat: add GitHub webhook route to handle incoming webhook events

--- Backend/src/routes/github.route.js
+++ Backend/src/routes/github.route.js
@@ -1,1 +1,8 @@
 import express from "express";
+import { handleWbhook } from "../controller/github.controller.js";
+
+const router = express.Router();
+
+router.post("/webhook",express.raw({ type: "application/json" }), handleWebhook);
+
+export default router
